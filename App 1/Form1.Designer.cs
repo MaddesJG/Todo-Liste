19b5f60f99afe8ef7e9ddf9fc01d26d67034d318
Matthias Jung <maddesjg@hotmail.de>
2019-04-12 08:15:54 +0200

    Add project files.

---
+++ App 1/Form1.Designer.cs
@@ -0,0 +1,292 @@
+﻿namespace App_1
+{
+    partial class Hauptfenster
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Hauptfenster));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.eventLog1 = new System.Diagnostics.EventLog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.Clear = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.speichernToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ladenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listeLeerenToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.menüToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.preferencesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.Location = new System.Drawing.Point(12, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(349, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(367, 29);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 22);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Übernehmen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.button1_MouseMove);
+            // 
+            // eventLog1
+            // 
+            this.eventLog1.SynchronizingObject = this;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.label1.Location = new System.Drawing.Point(133, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(174, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Zu erledigende Aufgaben eingeben";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 83);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox1.Size = new System.Drawing.Size(440, 251);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseClick);
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            this.listBox1.DisplayMemberChanged += new System.EventHandler(this.listBox1_DisplayMemberChanged);
+            this.listBox1.FormatStringChanged += new System.EventHandler(this.listBox1_FormatStringChanged);
+            this.listBox1.ValueMemberChanged += new System.EventHandler(this.listBox1_ValueMemberChanged);
+            this.listBox1.ClientSizeChanged += new System.EventHandler(this.listBox1_ClientSizeChanged);
+            this.listBox1.TabIndexChanged += new System.EventHandler(this.listBox1_TabIndexChanged);
+            this.listBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.listBox1_KeyPress);
+            this.listBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.listBox1_MouseDown);
+            // 
+            // Clear
+            // 
+            this.Clear.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.Clear.Location = new System.Drawing.Point(86, 57);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(84, 22);
+            this.Clear.TabIndex = 5;
+            this.Clear.Text = "Liste leeren";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Location = new System.Drawing.Point(367, 57);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(84, 22);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Speichern";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button4.Location = new System.Drawing.Point(176, 57);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(96, 22);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "Auswahl löschen";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 339);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(464, 22);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(13, 17);
+            this.toolStripStatusLabel1.Text = "0";
+            // 
+            // button5
+            // 
+            this.button5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button5.Location = new System.Drawing.Point(277, 57);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(84, 22);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Laden";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.speichernToolStripMenuItem,
+            this.ladenToolStripMenuItem,
+            this.listeLeerenToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.System;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(137, 70);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
+            // 
+            // speichernToolStripMenuItem
+            // 
+            this.speichernToolStripMenuItem.Name = "speichernToolStripMenuItem";
+            this.speichernToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.speichernToolStripMenuItem.Text = "Speichern";
+            this.speichernToolStripMenuItem.Click += new System.EventHandler(this.speichernToolStripMenuItem_Click);
+            // 
+            // ladenToolStripMenuItem
+            // 
+            this.ladenToolStripMenuItem.Name = "ladenToolStripMenuItem";
+            this.ladenToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.ladenToolStripMenuItem.Text = "Laden";
+            this.ladenToolStripMenuItem.Click += new System.EventHandler(this.ladenToolStripMenuItem_Click);
+            // 
+            // listeLeerenToolStripMenuItem
+            // 
+            this.listeLeerenToolStripMenuItem.Name = "listeLeerenToolStripMenuItem";
+            this.listeLeerenToolStripMenuItem.Size = new System.Drawing.Size(136, 22);
+            this.listeLeerenToolStripMenuItem.Text = "Liste Leeren";
+            this.listeLeerenToolStripMenuItem.Click += new System.EventHandler(this.listeLeerenToolStripMenuItem_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menüToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(464, 24);
+            this.menuStrip1.TabIndex = 10;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // menüToolStripMenuItem
+            // 
+            this.menüToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.preferencesToolStripMenuItem});
+            this.menüToolStripMenuItem.Name = "menüToolStripMenuItem";
+            this.menüToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.menüToolStripMenuItem.Text = "Menü";
+            // 
+            // preferencesToolStripMenuItem
+            // 
+            this.preferencesToolStripMenuItem.Name = "preferencesToolStripMenuItem";
+            this.preferencesToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.preferencesToolStripMenuItem.Text = "Preferences";
+            this.preferencesToolStripMenuItem.Click += new System.EventHandler(this.preferencesToolStripMenuItem_Click);
+            // 
+            // Hauptfenster
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(464, 361);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(480, 400);
+            this.Name = "Hauptfenster";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ToDo List";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Hauptfenster_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.eventLog1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Diagnostics.EventLog eventLog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem speichernToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ladenToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listeLeerenToolStripMenuItem;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem menüToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem preferencesToolStripMenuItem;
+    }
+}
+
---
+++ App 1/Form1.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace App_1
+{
+    
+
+    public partial class Hauptfenster : Form
+    {
+        int checkfile;
+
+
+        public Hauptfenster()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button1.Enabled = false;         
+            //button3.Enabled = false;
+
+
+           
+        }
+
+    
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                button1.Enabled = false;
+            }
+
+            else if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                button1.Enabled = true;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+
+           listBox1.Items.Add(textBox1.Text);
+           toolStripStatusLabel1.Text = Convert.ToString(listBox1.Items.Count);
+           textBox1.Clear();
+           textBox1.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            
+
+            listBox1.Items.Clear();
+            toolStripStatusLabel1.Text = Convert.ToString(listBox1.Items.Count);
+            textBox1.Focus();
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == 13)
+            {
+                if (button1.Enabled == true)
+                {
+                    listBox1.Items.Add(textBox1.Text);
+                    toolStripStatusLabel1.Text = Convert.ToString(listBox1.Items.Count);
+                    textBox1.Clear();
+                    //button1.Location = new Point(367, 29);
+
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+
+        private void toolStripStatusLabel2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel2_Click_1(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_ValueMemberChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_ClientSizeChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void troll_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+          
+        }
+
+        private void toolStripStatusLabel1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, MouseEventArgs e)
+        {
+
+        }
+
+        private void button1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            Random position1 = new Random();
+
+             if (textBox1.Text.ToLower() == "hau ab")
+            {
+                
+                //button1.Location = new Point(position1.Next(10, 380), position1.Next(10, 60));
+
+            }
+
+             else
+             {
+                 //button1.Location = new Point(367, 29);
+             }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+
+
+            SaveFileDialog sfd = new SaveFileDialog();
+
+           sfd.InitialDirectory = "c:\\";
+           sfd.Filter = "Todo Files (*.todo)|*.todo";
+           sfd.FilterIndex = 1;
+           sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+                
+                StreamWriter savefile = new StreamWriter(sfd.FileName);
+
+                foreach (var item in listBox1.Items)
+                {
+
+
+                    savefile.WriteLine(item.ToString());
+                }
+
+                
+
+                savefile.Close();
+
+                
+            }
+
+            checkfile = listBox1.Items.Count;
+                  
+                       
+    
+        }
+
+        private void listBox1_TabIndexChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void listBox1_DisplayMemberChanged(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void listBox1_FormatStringChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //listBox1.Items.Remove(listBox1.SelectedItem);
+
+
+
+            for (int x = listBox1.SelectedIndices.Count - 1; x >= 0; x--)
+            {
+                int idx = listBox1.SelectedIndices[x];
+                listBox1.Items.RemoveAt(idx);
+                
+            }
+
+            toolStripStatusLabel1.Text = Convert.ToString(listBox1.Items.Count);
+            
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+
+            
+
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+                 //ofd.InitialDirectory = "c:\\" ;
+                 ofd.Filter = "Todo Files (*.todo)|*.todo" ;
+                 ofd.FilterIndex = 1 ;
+                 ofd.RestoreDirectory = true ;
+
+                 
+                 
+
+                if(ofd.ShowDialog() == DialogResult.OK)
+                {
+                    listBox1.Items.Clear();
+                    StreamReader DateiLesen = new StreamReader(ofd.FileName);
+
+                    while (!DateiLesen.EndOfStream)
+                    {
+
+                        string zeile = DateiLesen.ReadLine();
+                        listBox1.Items.Add(zeile);
+                    }
+
+                    DateiLesen.Close();
+
+                 }
+
+
+
+                toolStripStatusLabel1.Text = Convert.ToString(listBox1.Items.Count);
+                checkfile = listBox1.Items.Count;
+            
+          
+        }
+
+        private void listBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            
+            if (e.KeyChar == 8)
+            {
+                for (int x = listBox1.SelectedIndices.Count - 1; x >= 0; x--)
+                {
+                    int idx = listBox1.SelectedIndices[x];
+                    listBox1.Items.RemoveAt(idx);
+                }
+
+                toolStripStatusLabel1.Text = Convert.ToString(listBox1.Items.Count);
+                
+            }
+                else
+                {
+
+                }
+            
+        }
+
+        private void toolStripStatusLabel1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void listBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                contextMenuStrip1.Show(Cursor.Position);
+
+            }
+            else
+            {
+                contextMenuStrip1.Hide();
+            }
+        }
+
+        private void ladenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            OpenFileDialog ofd = new OpenFileDialog();
+
+            //ofd.InitialDirectory = "c:\\" ;
+            ofd.Filter = "Todo Files (*.todo)|*.todo";
+            ofd.FilterIndex = 1;
+            ofd.RestoreDirectory = true;
+
+
+
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                listBox1.Items.Clear();
+                StreamReader DateiLesen = new StreamReader(ofd.FileName);
+
+                while (!DateiLesen.EndOfStream)
+                {
+
+                    string zeile = DateiLesen.ReadLine();
+                    listBox1.Items.Add(zeile);
+                }
+
+                DateiLesen.Close();
+
+            }
+
+
+
+            toolStripStatusLabel1.Text = Convert.ToString(listBox1.Items.Count);
+            checkfile = listBox1.Items.Count;
+
+        }
+
+        private void speichernToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+
+            //sfd.InitialDirectory = "c:\\";
+            sfd.Filter = "Todo Files (*.todo)|*.todo";
+            sfd.FilterIndex = 1;
+            sfd.RestoreDirectory = true;
+
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+
+
+                StreamWriter savefile = new StreamWriter(sfd.FileName);
+
+
+                foreach (var item in listBox1.Items)
+                {
+
+
+                    savefile.WriteLine(item.ToString());
+                }
+
+                savefile.Close();
+
+
+            }
+
+            checkfile = listBox1.Items.Count;
+
+        }
+
+        private void listeLeerenToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            toolStripStatusLabel1.Text = Convert.ToString(listBox1.Items.Count);
+            textBox1.Focus();
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripStatusLabel1_Click_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Hauptfenster_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (listBox1.Items.Count != checkfile)
+            {
+
+                var confirm = MessageBox.Show("Möchten sie speichern?", "Schließen", MessageBoxButtons.YesNoCancel);
+
+                if (confirm == DialogResult.Cancel)
+                {
+                    e.Cancel = true;
+
+                }
+
+                if (confirm == DialogResult.Yes)
+                {
+                    SaveFileDialog sfd = new SaveFileDialog();
+
+                    //sfd.InitialDirectory = "c:\\";
+                    sfd.Filter = "Todo Files (*.todo)|*.todo";
+                    sfd.FilterIndex = 1;
+                    sfd.RestoreDirectory = true;
+
+                    if (sfd.ShowDialog() == DialogResult.OK)
+                    {
+
+
+                        StreamWriter savefile = new StreamWriter(sfd.FileName);
+
+
+                        foreach (var item in listBox1.Items)
+                        {
+
+
+                            savefile.WriteLine(item.ToString());
+                        }
+
+                        savefile.Close();
+
+
+                    }
+
+                }
+
+                if (confirm == DialogResult.No)
+                {
+
+                }
+            }
+
+           
+        }
+
+        private void preferencesToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            
+        }
+
+   
+    }
+}
